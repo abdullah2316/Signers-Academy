c0889351ccf232f273a25404f32be7071e61867b
Zainab Fatima <zainabfatima255@gmail.com>
2023-01-01 17:28:41 +0500

    Dictionary Added

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-//import axios from "axios";
+import axios from "axios";
 import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 import { TextInput } from "react-native-paper";
 function Login({ navigation }) {
